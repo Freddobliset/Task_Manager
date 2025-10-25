3fae993eab1987bde6ccd8707d5e29ce52ca6593
Freddobliset <mariosale2004@gmail.com>
2025-10-25 20:27:41 +0200

    added help command that shows the list of usable commands

--- main.cpp
+++ main.cpp
@@ -5,42 +5,35 @@
 #include "edit_task.h"
 int main() {
 
-    int choice;
+    string choice;
+    cout << "use 'help' to see available commands.\n\n";
     do {
-        cout << "\n===== Task Manager =====\n";
-        cout << "1. Create new task\n";
-        cout << "2. Remove a task\n";
-        cout << "3. Show all tasks\n";
-        cout << "4. edit task\n";
-        cout << "0. Exit\n";
-        cout << "Choose an option: ";
-        cin >> choice;
-
-        switch (choice) {
-            case 1:
-                Taskcreation tc;
-                tc.createTask();
-                break;
-            case 2:
-                Removetask rt;
-                rt.removeTask();
-                break;
-            case 3:
-                Showtask st;
-                st.Showtasks();
-                break;
-            case 4:
-                EditTask et;
-                et.edit();
-                break;
-            case 0:
-                cout << "Exiting...\n";
-                break;
-            default:
-                cout << "Invalid option, try again.\n";
-                break;
+        getline(cin, choice);
+        if(choice == "create") {
+            Taskcreation tc;
+            tc.createTask();
+        } else if(choice == "remove") {
+            Removetask rm;
+            rm.removeTask();
+        } else if(choice == "show") {
+            Showtask st;
+            st.Showtasks();
+        } else if(choice == "edit") {
+            EditTask et;
+            et.edit();
+        } else if(choice == "exit") {
+            cout << "Exiting the program.\n";
+        }else if(choice == "help"){
+            cout << "Available commands:\n\n";
+            cout << "create - Create a new task\n\n";
+            cout << "remove - Remove an existing task by ID\n\n";
+            cout << "show - Display all current tasks\n\n";
+            cout << "edit - Edit an existing task by ID\n\n";
+            cout << "exit - Exit the program\n\n";
+        }else {
+            cout << "Invalid option. Please try again.\n";
         }
-    } while (choice != 0);
+    } while (choice != "exit");
 
     return 0;
 }
